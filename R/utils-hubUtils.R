5917cde996598c702d9ab85ccaa9049e0abe7a1a
Anna Krystalli <annakrystalli@googlemail.com>
2024-02-28 11:34:34 +0200

    Add functionality from hubUtils

---
+++ R/utils-hubUtils.R
@@ -0,0 +1,39 @@
+#' Convert model output to a `model_out_tbl` class object
+#'
+#' See [hubUtils::as_model_out_tbl()] for details.
+#'
+#' @name as_model_out_tbl
+#' @inherit hubUtils::as_model_out_tbl return examples
+#' @export
+#' @importFrom hubUtils as_model_out_tbl
+NULL
+
+#' Validate a `model_out_tbl` object
+#'
+#' See [hubUtils::validate_model_out_tbl()] for details.
+#'
+#' @name validate_model_out_tbl
+#' @inherit hubUtils::validate_model_out_tbl return examples
+#' @export
+#' @importFrom hubUtils validate_model_out_tbl
+NULL
+
+#' Merge model output tbl `model_id` column
+#'
+#' See [hubUtils::model_id_merge()] for details.
+#'
+#' @name model_id_merge
+#' @inherit hubUtils::model_id_merge return examples
+#' @export
+#' @importFrom hubUtils model_id_merge
+NULL
+
+#' Split model output tbl `model_id` column
+#'
+#' See [hubUtils::model_id_merge()] for details.
+#'
+#' @name model_id_split
+#' @inherit hubUtils::model_id_merge return examples
+#' @export
+#' @importFrom hubUtils model_id_split
+NULL
---
+++ data-raw/sysdata.R
@@ -0,0 +1,10 @@
+## code to prepare `std_col_datatypes` dataset goes here
+std_col_datatypes <- list(
+    model_id = c("character", "factor"),
+    output_type = c("character", "factor"),
+    value = "numeric"
+)
+
+usethis::use_data(std_col_datatypes,
+                  overwrite = TRUE, internal = TRUE
+)
