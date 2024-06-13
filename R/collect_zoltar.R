7e1a8fd0b1d162843585388804d53c10d00df645
matthewcornell <matt@matthewcornell.org>
2024-06-13 11:17:24 -0400

    WIP #10 [create a zoltar option in connect_hub() #10]

--- R/collect_zoltar.R
+++ R/collect_zoltar.R
@@ -2,7 +2,7 @@ yyyy_mm_dd_date_format <- "%Y-%m-%d"  # e.g., '2017-01-17'
 
 #' Load forecasts from zoltardata.com in hubverse format
 #'
-#' `collect_zoltar_forecasts` retrieves data from a \href{https://zoltardata.com/}{zoltardata.com} project and
+#' `collect_zoltar` retrieves data from a \href{https://zoltardata.com/}{zoltardata.com} project and
 #' transforms it from Zoltar's native download format into a hubverse one. Zoltar (documentation
 #' \href{https://docs.zoltardata.com/}{here}) is a pre-hubverse research project that implements a repository of model
 #' forecast results, including tools to administer, query, and visualize uploaded data, along with R and Python APIs to
@@ -10,26 +10,24 @@ yyyy_mm_dd_date_format <- "%Y-%m-%d"  # e.g., '2017-01-17'
 #' \href{https://github.com/reichlab/zoltpy/}{zoltpy}, respectively.) (This hubData function is itself implemented using
 #' the zoltr package.)
 #'
-#' @param project_name a string naming the Zoltar project to load forecasts from. assumes the host is zoltardata.com
-#' @param models a character vector that specifies the models to query. must be model abbreviations. defaults to NULL,
-#'   which queries all models in the project
-#' @param timezeros a character vector that specifies the timezeros to query. must be yyyy-mm-dd format. defaults to
-#'   NULL, which queries all timezeros in the project
-#' @param units a character vector that specifies the units to query. must be unit abbreviations. defaults to NULL,
-#'   which queries all units in the project
-#' @param targets a character vector that specifies the targets to query. must be target names. defaults to NULL,
-#'   which queries all targets in the project
-#' @param types a character vector that specifies the forecast types to query. Choices are "bin", "point", "sample",
-#'   "quantile", "mean", and "median". defaults to NULL, which queries all types in the project. note: while Zoltar
-#'   supports "named" and "mode" forecasts, this function ignores them
-#' @param as_of a datetime string that specifies the forecast version. The as_of field format must be a datetime as
-#'   parsed by the \href{https://dateutil.readthedocs.io/}{dateutil python library}, which accepts a variety of styles.
-#'   You can find examples \href{https://dateutil.readthedocs.io/en/stable/examples.html#parse-examples}{here}.
-#'   importantly, the datetime must include timezone information for disambiguation, without which the query will fail.
-#'   also, the datatime parsing function used below is extremely lenient when it comes to formatting, so please exercise
-#'   caution. defaults to NULL to load the latest version
-#' @param point_output_type a string that specifies how to convert zoltar `point` forecast data to hubverse output type.
-#'   must be either "median" or "mean". defaults to "median"
+#' @param project_name A string naming the Zoltar project to load forecasts from. Assumes the host is zoltardata.com .
+#' @param models A character vector that specifies the models to query. Must be model abbreviations. Defaults to NULL,
+#'   which queries all models in the project.
+#' @param timezeros A character vector that specifies the timezeros to query. Must be yyyy-mm-dd format. Defaults to
+#'   NULL, which queries all timezeros in the project.
+#' @param units A character vector that specifies the units to query. Must be unit abbreviations. Defaults to NULL,
+#'   which queries all units in the project.
+#' @param targets A character vector that specifies the targets to query. Must be target names. Defaults to NULL,
+#'   which queries all targets in the project.
+#' @param types A character vector that specifies the forecast types to query. Choices are "bin", "point", "sample",
+#'   "quantile", "mean", and "median". Defaults to NULL, which queries all types in the project. Note: While Zoltar
+#'   supports "named" and "mode" forecasts, this function ignores them.
+#' @param as_of A datetime string that specifies the forecast version. The datetime must include timezone information
+#'   for disambiguation, without which the query will fail. The datatime parsing function used below (`base::strftime`)
+#'   is extremely lenient when it comes to formatting, so please exercise caution. Defaults to NULL to load the latest
+#'   version.
+#' @param point_output_type A string that specifies how to convert zoltar `point` forecast data to hubverse output type.
+#'   Must be either "median" or "mean". Defaults to "median".
 #'
 #' @details
 #' Zoltar's data model differs from that of the hubverse in a few important ways. While Zoltar's model has the
@@ -41,22 +39,24 @@ yyyy_mm_dd_date_format <- "%Y-%m-%d"  # e.g., '2017-01-17'
 #' sample), only some of which overlap.
 #'
 #' Additional notes:
-#' * requires the user to have a Zoltar account (use the \href{https://zoltardata.com/about}{Zoltar contact page}
-#'   to request one)
-#' * requires `Z_USERNAME` and `Z_PASSWORD` environment vars to be set to those of the user's Zoltar account
-#' * while Zoltar supports "named" and "mode" forecasts, this function ignores them
-#' * rows with non-numeric values are ignored
-#' * this function removes numeric_horizon mentions from zoltar target names. target names can contain a maximum of
-#'   one numeric_horizon. example: "1 wk ahead inc case" -> "wk ahead inc case"
+#' * Requires the user to have a Zoltar account (use the \href{https://zoltardata.com/about}{Zoltar contact page}
+#'   to request one).
+#' * Requires `Z_USERNAME` and `Z_PASSWORD` environment vars to be set to those of the user's Zoltar account.
+#' * While Zoltar supports "named" and "mode" forecasts, this function ignores them.
+#' * Rows with non-numeric values are ignored.
+#' * This function removes numeric_horizon mentions from zoltar target names. Target names can contain a maximum of
+#'   one numeric_horizon. Example: "1 wk ahead inc case" -> "wk ahead inc case".
+#' * Querying a large number of rows may cause errors, so we recommend providing one or more filtering arguments
+#'   (e.g., models, timezeros, etc.) to limit the result.
 #'
-#' @return a hubverse model_out_tbl containing the following columns: "model_id", "timezero", "season", "unit",
-#'   "horizon", "target", "output_type", "output_type_id", and "value"
+#' @return A hubverse model_out_tbl containing the following columns: "model_id", "timezero", "season", "unit",
+#'   "horizon", "target", "output_type", "output_type_id", and "value".
 #' @export
 #'
 #' @examples \dontrun{
-#' df <- collect_zoltar_forecasts("Docs Example Project")
+#' df <- collect_zoltar("Docs Example Project")
 #' df <-
-#'   collect_zoltar_forecasts("Docs Example Project", models = c("docs_mod"),
+#'   collect_zoltar("Docs Example Project", models = c("docs_mod"),
 #'                         timezeros = c("2011-10-16"), units = c("loc1", "loc3"),
 #'                         targets = c("pct next week", "cases next week"), types = c("point"),
 #'                         as_of = NULL, point_output_type = "mean")
@@ -64,7 +64,7 @@ yyyy_mm_dd_date_format <- "%Y-%m-%d"  # e.g., '2017-01-17'
 #'
 #' @importFrom rlang .data
 #' @importFrom zoltr do_zoltar_query
-collect_zoltar_forecasts <- function(project_name, models = NULL, timezeros = NULL, units = NULL, targets = NULL,
+collect_zoltar <- function(project_name, models = NULL, timezeros = NULL, units = NULL, targets = NULL,
                                      types = NULL, as_of = NULL, point_output_type = "median") {
   zoltar_connection <- zoltr::new_connection()
   zoltr::zoltar_authenticate(zoltar_connection, Sys.getenv("Z_USERNAME"), Sys.getenv("Z_PASSWORD"))
@@ -90,7 +90,7 @@ collect_zoltar_forecasts <- function(project_name, models = NULL, timezeros = NU
 #' @importFrom rlang .data
 format_to_hub_model_output <- function(forecasts, zoltar_targets_df, point_output_type) {
   hub_model_outputs <- forecasts |>
-    dplyr::left_join(dplyr::select(zoltar_targets_df, .data$name, .data$numeric_horizon), by = c("target" = "name")) |>
+    dplyr::left_join(zoltar_targets_df[, c("name", "numeric_horizon")], by = c("target" = "name")) |>
     dplyr::mutate(numeric_value = ifelse(!is.na(.data$value),
                                          stringr::str_detect(.data$value, "[^\\d|\\.]", negate = TRUE),
                                          TRUE),
@@ -101,7 +101,7 @@ format_to_hub_model_output <- function(forecasts, zoltar_targets_df, point_outpu
     dplyr::mutate(value = as.numeric(.data$value)) |>
     dplyr::mutate(hub_target = stringr::str_squish(stringr::str_remove(.data$target, paste0(.data$numeric_horizon)))) |>
     dplyr::group_split(class) |>
-    purrr::map_dfr(.f = function(split_outputs) {
+    purrr::map(.f = function(split_outputs) {
       class <- split_outputs$class[1]
       if (class == "bin") {
         split_outputs |>
@@ -126,6 +126,7 @@ format_to_hub_model_output <- function(forecasts, zoltar_targets_df, point_outpu
           dplyr::ungroup()
       }
     }) |>
+    purrr::list_rbind() |>
     dplyr::filter(!is.na(.data$hub_value))
 
   # columns at this point:
@@ -133,9 +134,8 @@ format_to_hub_model_output <- function(forecasts, zoltar_targets_df, point_outpu
   # - numeric_horizon
   # - hub_target, output_type, output_type_id, hub_value
 
-  hub_model_outputs |>
-    dplyr::select(.data$model, .data$timezero, .data$season, .data$unit, .data$numeric_horizon, .data$hub_target,
-                  .data$output_type, .data$output_type_id, .data$hub_value) |>
+  hub_model_outputs[, c("model", "timezero", "season", "unit", "numeric_horizon", "hub_target", "output_type",
+                      "output_type_id", "hub_value")] |>
     dplyr::rename(model_id = .data$model, horizon = .data$numeric_horizon, target = .data$hub_target,
                   value = .data$hub_value)
 }
@@ -152,7 +152,7 @@ validate_arguments <- function(zoltar_connection, project_name, models, timezero
   if (!all(models %in% project_models$model_abbr)) {
     # nolint start
     missing_models <- setdiff(models, project_models)
-    cli::cli_abort("model(s) not found in project: {.val {missing_models}}")
+    cli::cli_abort("model{?s} not found in project: {.val {missing_models}}")
     # nolint end
   }
 
@@ -171,7 +171,7 @@ validate_arguments <- function(zoltar_connection, project_name, models, timezero
   if (!all(timezeros %in% project_timezeros)) {
     # nolint start
     missing_timezeros <- setdiff(timezeros, project_timezeros)
-    cli::cli_abort("timezero(s) not found in project: {.val {missing_timezeros}}")
+    cli::cli_abort("timezero{?s} not found in project: {.val {missing_timezeros}}")
     # nolint end
   }
 
@@ -179,7 +179,7 @@ validate_arguments <- function(zoltar_connection, project_name, models, timezero
   if (!all(units %in% project_units)) {
     # nolint start
     missing_units <- setdiff(units, project_units)
-    cli::cli_abort("unit(s) not found in project: {.val {missing_units}}")
+    cli::cli_abort("unit{?s} not found in project: {.val {missing_units}}")
     # nolint end
   }
 
@@ -187,7 +187,7 @@ validate_arguments <- function(zoltar_connection, project_name, models, timezero
   if (!all(targets %in% project_targets)) {
     # nolint start
     missing_targets <- setdiff(targets, project_targets)
-    cli::cli_abort("target(s) not found in project: {.val {missing_targets}}")
+    cli::cli_abort("target{?s} not found in project: {.val {missing_targets}}")
     # nolint end
   }
 
@@ -195,7 +195,7 @@ validate_arguments <- function(zoltar_connection, project_name, models, timezero
   if (!all(types %in% valid_types)) {
     # nolint start
     missing_types <- setdiff(types, valid_types)
-    cli::cli_abort("invalid type(s): {.val {missing_types}}")
+    cli::cli_abort("invalid type{?s}: {.val {missing_types}}")
     # nolint end
   }
 
