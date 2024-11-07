3e6a64e33c016340a5c5a521962c786af70fbe1d
Anna Krystalli <annakrystalli@googlemail.com>
2024-11-07 12:37:31 +0200

    Simplify re-export, remove examples and rename file

---
+++ R/reexports-hubUtils.R
@@ -0,0 +1,15 @@
+#' @export
+#' @importFrom hubUtils as_model_out_tbl
+hubUtils::as_model_out_tbl
+
+#' @export
+#' @importFrom hubUtils validate_model_out_tbl
+hubUtils::validate_model_out_tbl
+
+#' @export
+#' @importFrom hubUtils model_id_merge
+hubUtils::model_id_merge
+
+#' @export
+#' @importFrom hubUtils model_id_split
+hubUtils::model_id_split
